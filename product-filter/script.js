1bdd10ccd096d215d9e0ac2ffcf414e7b77c5daf
Aditya kumar <silvertimberwolf47@gmail.com>
2026-02-02 17:26:03 +0530

    Update script.js

--- product-filter/script.js
+++ product-filter/script.js
@@ -1,1 +1,18 @@
-// Product filter logic
+var filter = document.getElementById("filter");
+var products = document.getElementsByClassName("product");
+filter.onchange = function () {
+    var selectedValue = filter.value;
+    for (var i = 0; i < products.length; i++) {
+
+        var productType = products[i].getAttribute("data-type");
+        if (selectedValue == "all") {
+            products[i].style.display = "block";
+        }
+        else if (productType == selectedValue) {
+            products[i].style.display = "block";
+        }
+        else {
+            products[i].style.display = "none";
+        }
+    }
+};
